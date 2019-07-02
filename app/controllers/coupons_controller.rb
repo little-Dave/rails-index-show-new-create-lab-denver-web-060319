7fd406b7a670cd5f6c5e642a0132e82152087509
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-07-02 13:19:16 -0600

    Build out a Rails app that utilizes a form to create new objects
    Build RESTful 'index, 'show', 'new' and 'create' actions; utilize route helpers

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,29 @@
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.store = params["coupon"]["store"]
+    @coupon.coupon_code = params["coupon"]["coupon_code"]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  private
+
+  def allowed_params
+    params.require(:coupon).permit(:coupon_code, :store)
+  end
+
+end
